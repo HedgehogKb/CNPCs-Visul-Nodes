dfef4b4f24ef68b8e0f80b6b27e9e24b20cd55cf
BEREK HARRISON (879111) <879111@lcps.org>
2025-05-02 16:52:36 -0400

    playing aorund with node editor frame

--- src/main/java/com/hedgehogkb/DialogNodeEditorFrame.java
+++ src/main/java/com/hedgehogkb/DialogNodeEditorFrame.java
@@ -1,55 +1,81 @@
 package com.hedgehogkb;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Label;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 
 public class DialogNodeEditorFrame {
     private DialogNode dialogNode;
     private JFrame frame;
+    private JPanel mainPanel;
+    private JPanel leftPanel;
+    private JPanel rightPanel;
+    private JPanel topPanel;
+    
+    private JButton changeColorButton;
 
     public DialogNodeEditorFrame(DialogNode dialogNode) {
         this.dialogNode = dialogNode;
+        initializeComponents();
+        buildLayout();
+    }
 
-        this.frame = new JFrame("Dialog "+dialogNode.getTitle() + " - Id:" + dialogNode.getDialogId());
-        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setSize(800, 500);
-        this.frame.setResizable(false);
-        this.frame.setLayout(new BorderLayout());
+    private void initializeComponents() {
 
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(0,2));
-        mainPanel.setBackground(Color.GREEN);
+        // Initialize JFrame
+        this.frame = new JFrame("Dialog Id: "+dialogNode.getDialogId() + " - " + dialogNode.getTitle());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setSize(800, 500);
 
-        JPanel leftPanel = new JPanel();
-        leftPanel.setLayout(new GridLayout(0,2));
-        leftPanel.setBackground(Color.RED);
+        //Initialize the main panel which is divided in two parts,
+        this.mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(0, 2));
+        mainPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Dialog Node Editor"));
 
-        JPanel rightPanel = new JPanel();
-        rightPanel.setBackground(Color.BLUE);
-        
-        JPanel topPanel = new JPanel();
-        topPanel.setBackground(Color.YELLOW);
-        topPanel.setLayout(new GridLayout(0, 2));
-        topPanel.setPreferredSize(new Dimension(800, 50));
-        topPanel.add(new Label("Title: " + dialogNode.getTitle()), BorderLayout.WEST);
-        topPanel.add(new Label(dialogNode.getTitle() + "Title: "), BorderLayout.AFTER_LINE_ENDS);
+        // Initialize top panel for dialog node properties
+        this.topPanel = new JPanel();
+        topPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
-        
-        //rightPanel.setLayout();
+        gbc.gridheight = 1;
+        gbc.gridwidth = 2;
+
+        //gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        JLabel titleLabel = new JLabel("Title: ");
+        topPanel.add(titleLabel, gbc);
 
 
 
+        //Initialize left panel
+        this.leftPanel = new JPanel();
+        leftPanel.setLayout(new GridLayout(4, 2));
+        leftPanel.setBorder(BorderFactory.createEtchedBorder());
+
+        //Initialize right panel
+        this.rightPanel = new JPanel();
+        rightPanel.setLayout(new GridLayout(4, 2));
+        rightPanel.setBorder(BorderFactory.createEtchedBorder());
+
+
+    }
+
+    private void buildLayout() {
+        this.mainPanel.add(leftPanel, 0);
+        this.mainPanel.add(rightPanel, 1);
         this.frame.add(mainPanel, BorderLayout.CENTER);
         this.frame.add(topPanel, BorderLayout.NORTH);
-        //mainPanel.getLayout().addLayoutComponent(null, rightPanel);
-        mainPanel.add(leftPanel);
-        mainPanel.add(rightPanel);
 
         this.frame.setVisible(true);
     }
